2861e15322bbb4cd0f85c49e73634ea417bc7016
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-10-08 14:08:21 +0300

    Initial commit

---
+++ src/main/java/com/timelink/time_link/TimeLinkApplication.java
@@ -0,0 +1,13 @@
+package com.timelink.time_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timelink/time_link/TimeLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timelink.time_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
