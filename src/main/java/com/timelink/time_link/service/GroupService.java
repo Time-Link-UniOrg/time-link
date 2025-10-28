954d73452aa2bf10ea18e2e18b700356702e3b0e
Milena Angelova <164063109+MillaA2004@users.noreply.github.com>
2025-10-28 12:59:29 +0200

    service files

---
+++ src/main/java/com/timelink/time_link/service/GroupService.java
@@ -0,0 +1,32 @@
+package com.timelink.time_link.service;
+
+import com.timelink.time_link.model.Group;
+import com.timelink.time_link.repository.GroupRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class GroupService {
+
+    private final GroupRepository groupRepository;
+
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Group> getAllGroups() {
+        return groupRepository.findAll();
+    }
+
+    public Group getGroupById(Long id) {
+        return groupRepository.findById(id).orElse(null);
+    }
+
+    public Group saveGroup(Group group) {
+        return groupRepository.save(group);
+    }
+
+    public void deleteGroup(Long id) {
+        groupRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/timelink/time_link/service/StudentService.java
@@ -0,0 +1,32 @@
+package com.timelink.time_link.service;
+
+import com.timelink.time_link.model.Student;
+import com.timelink.time_link.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
